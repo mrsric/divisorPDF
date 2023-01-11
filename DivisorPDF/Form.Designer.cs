d96f701a410c683a216d9170a1772caaeee1340d
Marcos Ricardo de Souza <mrs.ric.hot@hotmail.com>
2023-01-11 10:20:29 -0300

    Add files via upload

---
+++ DivisorPDF/Form.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace DivisorPDF
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSelecionarArquivo = new System.Windows.Forms.Button();
+            this.txtFileSource = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTamanhoDivisor = new System.Windows.Forms.TextBox();
+            this.btnProcessar = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSelecionarArquivo);
+            this.groupBox1.Controls.Add(this.txtFileSource);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(561, 94);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Arquivo Origem";
+            // 
+            // btnSelecionarArquivo
+            // 
+            this.btnSelecionarArquivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelecionarArquivo.Location = new System.Drawing.Point(432, 36);
+            this.btnSelecionarArquivo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSelecionarArquivo.Name = "btnSelecionarArquivo";
+            this.btnSelecionarArquivo.Size = new System.Drawing.Size(100, 28);
+            this.btnSelecionarArquivo.TabIndex = 1;
+            this.btnSelecionarArquivo.Text = "Selecionar";
+            this.btnSelecionarArquivo.UseVisualStyleBackColor = true;
+            this.btnSelecionarArquivo.Click += new System.EventHandler(this.btnSelecionarArquivo_Click);
+            // 
+            // txtFileSource
+            // 
+            this.txtFileSource.Location = new System.Drawing.Point(25, 38);
+            this.txtFileSource.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFileSource.Name = "txtFileSource";
+            this.txtFileSource.ReadOnly = true;
+            this.txtFileSource.Size = new System.Drawing.Size(397, 23);
+            this.txtFileSource.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(127, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Página(s):";
+            // 
+            // txtTamanhoDivisor
+            // 
+            this.txtTamanhoDivisor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTamanhoDivisor.Location = new System.Drawing.Point(204, 25);
+            this.txtTamanhoDivisor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTamanhoDivisor.Name = "txtTamanhoDivisor";
+            this.txtTamanhoDivisor.Size = new System.Drawing.Size(81, 23);
+            this.txtTamanhoDivisor.TabIndex = 2;
+            this.txtTamanhoDivisor.Text = "0";
+            this.txtTamanhoDivisor.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtTamanhoDivisor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTamanhoDivisor_KeyPress);
+            // 
+            // btnProcessar
+            // 
+            this.btnProcessar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcessar.Location = new System.Drawing.Point(324, 23);
+            this.btnProcessar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnProcessar.Name = "btnProcessar";
+            this.btnProcessar.Size = new System.Drawing.Size(100, 28);
+            this.btnProcessar.TabIndex = 1;
+            this.btnProcessar.Text = "Processar";
+            this.btnProcessar.UseVisualStyleBackColor = true;
+            this.btnProcessar.Click += new System.EventHandler(this.btnProcessar_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(522, 201);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(55, 15);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "MRSRIC";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnProcessar);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtTamanhoDivisor);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(16, 123);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(561, 70);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Divisor";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 225);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(610, 272);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(610, 272);
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Divisor PDF";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnProcessar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTamanhoDivisor;
+        private System.Windows.Forms.Button btnSelecionarArquivo;
+        private System.Windows.Forms.TextBox txtFileSource;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ DivisorPDF/Form.cs
@@ -0,0 +1,183 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace DivisorPDF
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+
+            //Embedded DLL
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+            {
+                string resourceName = new AssemblyName(args.Name).Name + ".dll";
+                string resource = Array.Find(this.GetType().Assembly.GetManifestResourceNames(), element => element.EndsWith(resourceName));
+
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource))
+                {
+                    Byte[] assemblyData = new Byte[stream.Length];
+                    stream.Read(assemblyData, 0, assemblyData.Length);
+                    return Assembly.Load(assemblyData);
+                }
+            };
+        }
+
+        private void btnProcessar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(openFileDialog1.FileName) || !openFileDialog1.FileName.ToUpper().EndsWith(".PDF"))
+            {
+                MessageBox.Show("Selecione um PDF", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);                
+            }
+            else
+            {
+                //Quantidade Paginas
+                PdfReader reader = new PdfReader(openFileDialog1.FileName);
+                int qtdPaginas = reader.NumberOfPages;
+                reader.Close();
+                int tamanhoDivisor = Convert.ToInt32(txtTamanhoDivisor.Text);
+
+                bool validacao = true;
+
+                if (tamanhoDivisor == 0)
+                {
+                    MessageBox.Show($"Informe a quantidade de página(s) do Divisor", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    validacao = false;
+                }
+
+                if (qtdPaginas == tamanhoDivisor && validacao)
+                {
+                    MessageBox.Show($"O PDF selecionado contém somente {qtdPaginas} pág.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    validacao = false;
+                }
+
+                if (qtdPaginas < tamanhoDivisor && validacao)
+                {
+                    MessageBox.Show($"O PDF selecionado contem menos pág. do que o divisor", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    validacao = false;
+                }
+
+                if (validacao)
+                {
+                    Dictionary<string, string> resultado = DivideArquivo(openFileDialog1.FileName, tamanhoDivisor);
+                    MessageBox.Show($"{resultado.Count} Arquivo(s) criado(s) com sucesso! \n\nDiretório: {Path.GetDirectoryName(openFileDialog1.FileName)}", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+         
+        }
+
+        private void btnSelecionarArquivo_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Pdf Files|*.pdf";
+            openFileDialog1.DefaultExt = ".pdf";
+            openFileDialog1.FileName = "";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFileSource.Text = openFileDialog1.FileName;
+            };
+
+        }
+        private Dictionary<String, String> DivideArquivo(string arquivo, int paginaDivisao)
+        {
+            Dictionary<String, String> arquivoDividido = new Dictionary<String, String>();
+
+            try
+            {                
+
+                string arquivoSaida =
+                        Path.GetDirectoryName(arquivo)
+                        + @"\"
+                        + Path.GetFileNameWithoutExtension(arquivo)
+                        + "-" + 1
+                        + Path.GetExtension(arquivo);
+                string[] InFiles = new string[] { arquivo };
+                
+                Merge(InFiles, arquivoSaida, false);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro na divisão entre em contato com a TI. Erro: {ex.Message}", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return arquivoDividido;
+        }
+        private void txtTamanhoDivisor_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) 
+                )
+            {
+                e.Handled = true;
+            }
+        }
+
+        public string Merge(string[] InFiles, string newFile, bool delete)
+        {
+            var arquivo = "";
+            string OutFile = "";
+
+            try
+            {
+                if (InFiles.Length > 0)
+                {
+                    int totalFiles = InFiles.GetLength(0);
+                    for (int i = 0; i < totalFiles; i++)
+                    {
+                        arquivo = System.IO.Path.GetFileName(InFiles[i]); 
+                    }
+
+                    if (!String.IsNullOrEmpty(newFile)) OutFile = newFile;
+                    else OutFile = InFiles[0].Substring(0, InFiles[0].Length - 4) + "-Merge.pdf";
+
+                    using (FileStream stream = new FileStream(OutFile, FileMode.Create))
+                    {
+                        using (Document doc = new Document())
+                        {
+                            using (PdfCopy pdf = new PdfSmartCopy(doc, stream))
+                            {
+                                doc.Open();
+                                PdfReader reader = null;
+                                PdfReader.unethicalreading = true;
+                               
+                                int total = InFiles.GetLength(0);
+                                for (int i = 0; i < total; i++)
+                                {
+                                    arquivo = System.IO.Path.GetFileName(InFiles[i]);
+                                    reader = new PdfReader(InFiles[i]);                               
+                                    pdf.AddDocument(reader);
+                                    pdf.FreeReader(reader);
+                                    reader.Close();
+                                    if (delete) File.Delete(InFiles[i]);
+                                }
+                            }
+                        }
+                    }
+                    if (String.IsNullOrEmpty(newFile))
+                    {
+                        File.Delete(InFiles[0]);
+                        File.Move(OutFile, InFiles[0]);
+                        OutFile = InFiles[0];
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new FileLoadException($"Erro ao realizar merge do arquivo [{arquivo}]. Erro: {e.Message}");
+            }
+            return OutFile;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            var url = "https://github.com/mrsric";
+            System.Diagnostics.Process.Start(url);
+        }
+    }
+}
